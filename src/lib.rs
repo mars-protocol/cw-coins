e70d3cdabed4cbff7ad9a60162115c8ae42b5a49
larry <26318510+larry0x@users.noreply.github.com>
2022-08-05 17:03:14 +0100

    add comment

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,24 @@ use serde::{de, Serialize};
 #[derive(Serialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Coins(pub BTreeMap<String, Uint128>);
 
+// We implement a custom serde::de::Deserialize trait to handle the case where the JSON string contains
+// duplicate keys, i.e. duplicate coin denoms.
+//
+// If we derive the trait, by default, it will not throw an error in such as case. Instead, it takes
+// the amount that is seen the latest. E.g. the following JSON string
+//
+// ```json
+// {
+//    "uatom": "12345",
+//    "uatom", "23456",
+//    "uatom": "67890"
+// }
+// ```
+//
+// will be deserialized into a Coins object with only one element, with denom `uatom` and amount 67890.
+// The amount 67890 is seen the latest and overwrites the two amounts seen earlier.
+//
+// This is NOT a desirable property. We want an error to be thown if the JSON string contain dups.
 impl<'de> de::Deserialize<'de> for Coins {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
